580b4b44ba5cae8deed2fdd59f24dd74b25a2252
Schandroid243 <schadrackngunza@gmail.com>
2022-10-03 10:08:03 +0100

    Learning how to consume an API with django in the basic.py file

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,11 @@
 import requests
 
 
-endpoint = 'https://httpbin.org/status/200/'
-endpoint = 'https://wwww.httpbin.org/anything'
+#endpoint = 'https://httpbin.org/status/200/'
+endpoint = 'https://httpbin.org/anything'
 
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, json={"query": "Hello world !"})
+
+print(get_response.json())
+print(get_response.status_code)
