3d91f606cbc9e6ad9c7f29c35c47e6a9d7ce4054
Schandroid243 <schadrackngunza@gmail.com>
2022-10-03 10:54:46 +0100

    Added api app Added product app Added product models Added product migration Already run all the migrations to the database

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-17cp!^hpr(4zqqb4328r%y8%px9o(#ts0hv67%&6gp)7gh3o$h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'products'
 ]
 
 MIDDLEWARE = [
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,10 +2,12 @@ import requests
 
 
 #endpoint = 'https://httpbin.org/status/200/'
-endpoint = 'https://httpbin.org/anything'
+#endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/api/'
 
 
-get_response = requests.get(endpoint, json={"query": "Hello world !"})
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world !"})
 
 print(get_response.json())
 print(get_response.status_code)
+#print(get_response.text)
